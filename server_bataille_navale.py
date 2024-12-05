d137ee474be092970a2a0d32d8625811b5c6909f
Dloyr <dimitriloyer@outlook.com>
2024-12-05 22:15:21 +0100

    fix bug sur le traitement des coordonées

--- server_bataille_navale.py
+++ server_bataille_navale.py
@@ -6,7 +6,7 @@ Dimitri Loyer
 Yasmine Sassi
 """
 
-from bataille_navale import shootAt, mkGridFromString
+from bataille_navale import shootAt, mkGridFromString, updateShoot
 from socket import *
 from threading import *
 from time import sleep # pour mettre un délais d'attente afin de laisser le temps au serveur d'envoyer les messages
@@ -17,6 +17,29 @@ tour_joueur = 1
 lock = Lock() # permet la synchronisation entre les clients
 clients = []
 grilles_des_joueurs =  {}
+
+def resultat_tir(joueur, coord_tir, client_actuel, client_adverse):
+    global grilles_des_joueurs
+    x, y = coord_tir
+    grille_adversaire = list(grilles_des_joueurs[3 - joueur])  # Convertir la chaîne en liste
+    index = y * 10 + x
+
+    if grille_adversaire[index] == ".":
+        # Raté
+        client_actuel.sendall("Raté...".encode("utf-8"))
+        client_adverse.sendall(f"L'adversaire a tiré sur ({x}, {y}) et a raté.".encode("utf-8"))
+    elif grille_adversaire[index] != "X":
+        # Touché
+        bateau_touche = grille_adversaire[index]
+        grille_adversaire[index] = "X"  # Marquer le tir
+        grilles_des_joueurs[3 - joueur] = ''.join(grille_adversaire)  # Mettre à jour la grille dans le dictionnaire
+        client_actuel.sendall("Touché !".encode("utf-8"))
+        sleep(1)
+        client_adverse.sendall(f"Votre bateau '{bateau_touche}' a été touché !".encode("utf-8"))
+        sleep(1)
+
+    grille_mise_a_jour = ''.join(grille_adversaire)
+    client_adverse.sendall(grille_mise_a_jour.encode("utf-8"))
 
 def ajuster_grille(grille):
     """
@@ -58,25 +81,7 @@ def gestion_client(client_du_serveur, joueur):
 
     # Réception et sauvegarde de la grille
     grille_recue = client_du_serveur.recv(1024).decode()
-    grille = mkGridFromString(grille_recue)
-    grille = ajuster_grille(grille)
-
-    # Initialisation des bateaux
-    bateaux = {}
-    for y in range(len(grille)):
-        ligne = grille[y]
-        for x in range(len(ligne)):
-            cell = ligne[x]
-            if cell != ".":
-                if cell not in bateaux:
-                    bateaux[cell] = {"positions": [], "hits": []}
-                bateaux[cell]["positions"].append((y, x))
-
-    grilles_des_joueurs[joueur] = {
-        "grid": grille,
-        "history": [["." for _ in range(10)] for _ in range(10)],
-        "boat": bateaux
-    }
+    grilles_des_joueurs[joueur] = grille_recue
 
     print("La grille du joueur {:d} a été sauvegardée.".format(joueur))
     if len(grilles_des_joueurs) == 2:
@@ -108,20 +113,8 @@ def gestion_client(client_du_serveur, joueur):
             client_du_serveur.send("Coordonnées invalides.".encode("utf-8"))
             continue
 
-        # Appel à la fonction `shootAt`
-        resultat_tour = shootAt(
-            grilles_des_joueurs[joueur],  # Le joueur qui tire
-            grilles_des_joueurs[3 - joueur],  # L'adversaire
-            coord_tir_tuple
-        )
-
-        # Envoi du résultat au client
-        if resultat_tour == 0:
-            client_du_serveur.send("Raté !".encode("utf-8"))
-        elif resultat_tour == 1:
-            client_du_serveur.send("Touché !".encode("utf-8"))
-        elif resultat_tour == 2:
-            client_du_serveur.send("Coulé !".encode("utf-8"))
+        # Appel à la fonction resultat_tir
+        resultat_tir(joueur, coord_tir_tuple, client_du_serveur, autre_client)
 
         with lock:
             tour_joueur = 3 - joueur
